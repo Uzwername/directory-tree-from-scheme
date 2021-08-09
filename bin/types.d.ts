bc7c53253767ab458058b93def61ff0cf2ae2ce4
Igor Bykov <igor.bykov@protonmail.com>
2021-08-09 07:57:02 +0200

    New: Add unescape-js

---
+++ bin/types.d.ts
@@ -0,0 +1,8 @@
+declare module "unescape-js" {
+  /**
+   * Unescape special characters encoded with JavaScript escape sequences
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#using_special_characters_in_strings}
+   */
+  function unescapeJs (string: string): string
+  export = unescapeJs;
+}
